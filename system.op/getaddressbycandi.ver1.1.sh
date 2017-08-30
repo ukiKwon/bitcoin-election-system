e7675d33f649dae566062886fc1668910022653c
snrndi121 <snrndi4521@gmail.com>
2017-08-30 18:38:24 +0900

    kweb.ver1.2.01 updataed

--- system.op/getaddressbycandi.ver1.1.sh
+++ system.op/getaddressbycandi.ver1.1.sh
@@ -5,9 +5,9 @@
 declare -a addr
 declare -i ACCOUNT_REGION
 
-declare -i CUR_ACCOUNT_NUM #the number of address in a account 
-declare -i REQ_ACCOUNT_NUM #the number requested to generated newly 
-declare -i GEN_ACCOUNT_NUM #the number going to be generated  
+declare -i CUR_ACCOUNT_NUM #the number of address in a account
+declare -i REQ_ACCOUNT_NUM #the number requested to generated newly
+declare -i GEN_ACCOUNT_NUM #the number going to be generated
 declare -a CANDIDATE_LIST #the argument list of candidates
 
 ACCOUNT_NAME="candidates"
@@ -22,11 +22,11 @@ cmd_make='bitcoin-cli -regtest -datadir=/home/uki408/Documents/bitcoin/src/uki g
 cmd_list='bitcoin-cli -regtest -datadir=/home/uki408/Documents/bitcoin/src/uki getaddressesbyaccount ' #3.count the number of account.
 cmd_account='bitcoin-cli -regtest -datadir=/home/uki408/Documents/bitcoin/src/uki listaccounts '
 
-#.count the account of a wallet. 
+#.count the account of a wallet.
 function count_account() { #(working)
 	#echo -e "--- count_account ---"
-	declare -i _num_account 
-	declare -a _list_account 
+	declare -i _num_account
+	declare -a _list_account
 	$cmd_account
 	_list_account=$?
 	_num_account=${#list_account[@]}
@@ -34,7 +34,7 @@ function count_account() { #(working)
 	CUR_ACCOUNT_NUM=$_num_account
 	return $_num_account
 }
-# calculate the num of accounts to be generated 
+# calculate the num of accounts to be generated
 function calNum_accounts() { #(working)
 	declare -i _res
 	declare -a _accounts
@@ -50,11 +50,11 @@ function calNum_accounts() { #(working)
 #count the address of sepecified candidate.
 function count_addr() {
 	#echo -e "--- count_addr ---\n"
-	declare -i list_count 
+	declare -i list_count
 	_account_name=$1
 	_list_addr=($($cmd_list)$_account_name)
 	#list_addr=($($cmd_list))
-	_list_count=${#_list_addr[@]}	
+	_list_count=${#_list_addr[@]}
 	#echo "list_count :$list_count"
 	return $_list_count
 }
@@ -62,12 +62,12 @@ function count_addr() {
 function checkDup() {
 	#echo -e "--- checkDup ---\n"
 	_account=$1
-	_list_account=`$cmd_account | cut -d '"' -f2 -s` #JSON to string 
+	_list_account=`$cmd_account | cut -d '"' -f2 -s` #JSON to string
 	_list_account=($_list_account) #string to array
     for val in "${_list_account[@]}";do
 		#printf "comparing::$_account with $val \n"
 		if [ "$_account" == "$val" ];then
-			echo "true"	#alreay 
+			echo "true"	#alreay
 		fi
 	done
 	echo "false" #can be registered
@@ -76,15 +76,16 @@ function checkDup() {
 function gen_addr() {
 	echo -e "--- gen_addr ---\n"
 	declare -a ADDR_CANDI
-	declare -i i=0	
+	declare -i i=0
 	_list_accounts=$@ #the requested accounts
 	_list_accounts=($_list_accounts) #str to Array
-	#echo -e "GEN::the $_num_account_generated will be generated\n" 
+	#echo -e "GEN::the $_num_account_generated will be generated\n"
 	echo -e "GEN::name_list are :${_list_accounts[@]} \n"
+	echo -e "GEN::name_list num is :${#_list_accounts[@]} \n"
 
 	# generate addresses by accounts
-	for _new_account in "${_list_accounts[@]}";do
-		if [ $i -gt $GEN_ACCOUNT_NUM ];then break; fi 
+	for _new_account in ${_list_accounts[@]};do
+		if [ $i -gt $GEN_ACCOUNT_NUM ];then break; fi
 		_isDup=$( checkDup "$_new_account" )
 		echo -e "Dup? -> $_isDup " #the function checkDup is started.
 		#Generate addreses by a account
@@ -93,7 +94,7 @@ function gen_addr() {
 			_makeByaccount=$cmd_make$_new_account
 			echo "-------------- The New addresses List below -----------------"
 			for((i=0; i<$ACCOUNT_REGION; ++i));do
-				addr=`$_makeByaccount`		
+				addr=`$_makeByaccount`
 				echo ">> The newest ${ADDR_CANDI[$i]}=$addr"
 			done
 			#echo -e "${ADDR_CANDI[@]}\n"
@@ -109,17 +110,17 @@ function del_addr() {
 }
 function _update() {
 	echo "update is started now"
-	
+
 }
 function update_account() {
-	echo -e "Requesting = $REQ_ACCOUNT_NAME, Current = $CUR_ACCOUNT_NAME\n"	
+	echo -e "Requesting = $REQ_ACCOUNT_NAME, Current = $CUR_ACCOUNT_NAME\n"
 	echo -e "Updating can be started from the CURRENT to REQUESTING.(yes/no)"
 	read chk
 	case $chk in
 	yes|Yes|y)
 		`sudo -s`
 		if [ "$?" -eq 0 ];then
-			_update 
+			_update
 		else
 			echo ">> you have no permission to access. Bye"
 		fi
@@ -129,13 +130,13 @@ function update_account() {
 		;;
 	*) echo -e ">> You are wrong command. \n";;
 	esac
-	
+
 }
 #
 # -------------------------------------MAIN ----------------------------------------
 #
 #create candidate address by the num of candidate($1)
-echo -e " configuration candidate addresses .........\n\n" 
+echo -e " configuration candidate addresses .........\n\n"
 
 REQ_ACCOUNT_NUM=$#
 CANDIDATE_LIST=$@
@@ -152,9 +153,9 @@ if [ $REQ_ACCOUNT_NUM -gt 0 ] ;then		#Yes request
 	if [ $GEN_ACCOUNT_NUM -gt 0 ];then #generate new addresses
 		gen_addr ${CANDIDATE_LIST[@]}
 	elif [ $GEN_ACCOUNT_NUM -lt 0 ];then #too much addresses
-		del_addr		
+		del_addr
 	else	#enough address
-		echo -e ">> Now we have enough addresses\n"		
+		echo -e ">> Now we have enough addresses\n"
 		update_account
 	fi
 else	#No request
@@ -162,4 +163,3 @@ else	#No request
 fi
 # Main Process exit
 echo -e ">> There's nothing to do, generating Mode is done.\n >> Bye"
-		
